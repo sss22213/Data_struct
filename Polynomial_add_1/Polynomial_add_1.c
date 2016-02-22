c6d8e9265118960ca0a7adc3f6e62f17ec135801
sss22213 <n0404.n0404@gmail.com>
2016-02-22 22:23:29 +0800

    20160222

---
+++ Polynomial_add_1/Polynomial_add_1.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct _Polynomial Polynomial;
+typedef struct _term term;
+
+//Polynomial
+typedef struct _Polynomial
+{
+	int start;
+	int finish;
+	term *items[30];
+}Polynomial;
+
+//term
+typedef struct _term
+{
+        float coef;
+        int exp;
+}term;
+
+void add_term_items(term **array,int length,Polynomial *Pol)
+{
+	if(Pol->finish>=length)
+	{
+		perror("The length is too long");
+		exit(1);
+	}
+	for(int i=Pol->start;i<=Pol->finish-Pol->start;i++)
+	{
+		if(array[i]==NULL)
+		{
+			perror("The items is NULL");
+			exit(1);
+		}
+		array[i]=Pol->items[i];
+	}
+}
+
+int main()
+{
+	//term array
+	term *array[100];
+	for(int i=0;i<100;i++)
+	{
+		array[i]=(term*)malloc(sizeof(array[i]));
+		if(array[i]==NULL)
+		{
+			perror("Heap leak");
+                        exit(1);
+		}
+	}
+	//A Polynomial
+	Polynomial *A;
+	A=(Polynomial*)malloc(sizeof(A));
+	A->start=0;
+	A->finish=19;
+	for(int i=0;i<20;i++)
+	{
+		A->items[i]=(term*)malloc(sizeof(A->items[i]));
+		A->items[i]->coef=i*0.5;
+		A->items[i]->exp=i;	
+	}
+	//B Polynomial
+	Polynomial *B;
+	B=(Polynomial*)malloc(sizeof(A));
+        B->start=2;
+        B->finish=5;
+	//implement
+	add_term_items(array,20,A);
+	return 0;
+
+}
