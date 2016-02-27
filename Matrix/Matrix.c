a7679373d9c30f2c04ff8354a834d5712c976152
sss22213 <n0404.n0404@gmail.com>
2016-02-28 00:32:33 +0800

    add add_items

--- Matrix/Matrix.c
+++ Matrix/Matrix.c
@@ -7,6 +7,18 @@ typedef struct _SparseMat
 	int value;
 }SparseMat;
 
+void add_items(SparseMat *Mat,int Mat_len,int row,int col,int value)
+{
+	if(row*col>Mat_len)
+	{
+		perror("The row and col is bigger than Mat");
+		exit(1);
+	}
+	Mat->row=row;
+	Mat->col=col;
+	Mat->value=value;			
+}
+
 int main()
 {
 	SparseMat *A[10];
@@ -19,6 +31,14 @@ int main()
 			exit(1);
 		}
 	}
+	add_items(A[0],10,0,0,15);
+	add_items(A[1],10,0,3,22);
+	add_items(A[2],10,0,5,-15);
+	add_items(A[3],10,1,1,11);
+	add_items(A[4],10,1,2,3);
+	add_items(A[5],10,102,3,-6);
+	add_items(A[6],10,4,0,91);
+	add_items(A[7],10,5,2,28);
 	
 	return 0;
 }
